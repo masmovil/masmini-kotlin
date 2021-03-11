f456b82ad6f6e0063ef6db126e5d01d6f129924d
Francisco García Sierra <frangsierra@gmail.com>
2021-03-11 19:56:04 +0100

    WIP

--- masmini-common/src/main/java/masmini/LoggerInterceptor.kt
+++ masmini-common/src/main/java/masmini/LoggerInterceptor.kt
@@ -1,69 +1,77 @@
 package masmini
 
+import android.util.Log
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.min
 
-/** Actions implementing this interface won't log anything */
-@Action interface SilentAction
+/** Actions implementing this interface won't log anything, including nested calls */
+interface SilentAction
 
-class LoggerInterceptor constructor(stores: Collection<Store<*>>,
-                                    private val logFn: (tag: String, msg: String) -> Unit,
-                                    private val logInBackground: Boolean = false,
-                                    private val tag: String = "MiniLog") : Interceptor {
+/** Actions implementing this interface will log nested actions visually */
+interface SagaAction
 
-    private val coroutineDispatcher by lazy { Executors.newSingleThreadExecutor().asCoroutineDispatcher() }
+/**
+ * Action logging for stores.
+ */
+@Action
+class LoggerMiddleware(stores: Collection<StateContainer<*>>,
+                       private val tag: String = "MiniLog",
+                       private val diffFunction: ((a: Any?, b: Any?) -> String)? = null,
+                       private val logger: (priority: Int, tag: String, msg: String) -> Unit) :
+
+        Middleware {
+
+    private var actionCounter = AtomicInteger(0)
+
     private val stores = stores.toList()
-    private var lastActionTime = System.currentTimeMillis()
-    private var actionCounter: Long = 0
 
-    override fun invoke(action: Any, chain: Chain): Any {
-        if (action is SilentAction) return chain.proceed(action) //Do nothing
+    override suspend fun intercept(action: Any, chain: Chain): Any {
+        if (action is SilentAction) chain.proceed(action) //Do nothing
 
+        val isSaga = action is SagaAction
         val beforeStates: Array<Any?> = Array(stores.size) { Unit }
         val afterStates: Array<Any?> = Array(stores.size) { Unit }
 
         stores.forEachIndexed { idx, store -> beforeStates[idx] = store.state }
-        val start = System.currentTimeMillis()
-        val timeSinceLastAction = min(start - lastActionTime, 9999)
-        lastActionTime = start
-        actionCounter++
+
+        val (upCorner, downCorner) = if (isSaga) {
+            "╔═════ " to "╚════> "
+        } else {
+            "┌── " to "└─> "
+        }
+
+        val prelude = "[${"${actionCounter.getAndIncrement() % 100}".padStart(2, '0')}] "
+
+        logger(Log.DEBUG, tag, "$prelude$upCorner$action")
 
         //Pass it down
-        val out = chain.proceed(action)
-        val processTime = System.currentTimeMillis() - start
+        val start = System.nanoTime()
+        val outAction = chain.proceed(action)
+        val processTime = (System.nanoTime() - start) / 1000000
+
         stores.forEachIndexed { idx, store -> afterStates[idx] = store.state }
 
-        val logRunnable = Runnable {
-            val sb = StringBuilder()
-            sb.append(" \n")
-            sb.append("┌────────────────────────────────────────────\n")
-            sb.append(String.format("├─> %s %dms [+%dms][%d] - %s",
-                action.javaClass.simpleName, processTime, timeSinceLastAction, actionCounter % 10, action))
-                .append("\n")
-
+        if (!isSaga) {
             for (i in beforeStates.indices) {
                 val oldState = beforeStates[i]
                 val newState = afterStates[i]
                 if (oldState !== newState) {
-                    //This operation is costly, don't do it in prod
-                    val line = "${stores[i].javaClass.simpleName}: $newState"
-                    sb.append(String.format("│   %s", line)).append("\n")
+                    val line = "$prelude│ ${stores[i].javaClass.simpleName}"
+                    logger(Log.VERBOSE, tag, "$line: $newState")
+                    diffFunction?.invoke(oldState, newState)?.let { diff ->
+                        logger(Log.DEBUG, tag, "$line: $diff")
+                    }
                 }
             }
-            sb.append("└────────────────────────────────────────────\n")
-            logFn(tag, sb.toString())
         }
 
-        if (logInBackground) {
-            GlobalScope.launch(coroutineDispatcher) {
-                logRunnable.run()
-            }
-        } else {
-            logRunnable.run()
-        }
-        return out
+        logger(Log.DEBUG, tag, "$prelude$downCorner" +
+                "${action.javaClass.simpleName} ${processTime}ms")
+
+        return outAction
     }
 }
